ff1bc9d88d13f5d0785a0146fd81f6ee2698247a
zarandya <zarandya@githib.com>
2018-06-25 11:41:43 +0100

    webpage is now working and beautiful

--- workspace/Server.ts
+++ workspace/Server.ts
@@ -7,6 +7,20 @@ import log4js = require('log4js');
 
 const logger = log4js.getLogger();
 
+function closestStops(req, res) {
+    return getLocationOfPostcode(req.query['postcode'])
+        .then(location => findStopPointsNearLocation(location, 500))
+        .then(stops => stops.slice(0, 2).map(stop => new StopPoint(stop)))
+        .then(stopPoints => stopPoints.map(getNameAndBusesPromises))
+        .then(stopPointPromises => Promise.all(stopPointPromises))
+        .then(outputBusArray => {
+            logger.debug(outputBusArray)
+            res.type("json");
+            res.send((outputBusArray));
+        })
+        .catch(err => res.send(err))
+}
+
 export function startServer() {
     const app = express();
     const rootFolder = 'C:\\Users\\ALZ\\IdeaProjects\\BusBoard';
@@ -15,25 +29,15 @@ export function startServer() {
     };
 
     app.get('/', (req, res) => res.send('Hello World!'));
-    app.get('/closestStops', (req, res) => {
-
-        getLocationOfPostcode(req.query['postcode'])
-            .then(location => findStopPointsNearLocation(location, 500))
-            .then(stops => stops.slice(0, 2).map(stop => new StopPoint(stop)))
-            .then(stopPoints => stopPoints.map(getNameAndBusesPromises))
-            .then(stopPointPromises => Promise.all(stopPointPromises))
-            .then(outputBusArray => {
-                logger.debug(outputBusArray)
-                res.type("json");
-                res.send(JSON.stringify(outputBusArray));
-            })
-            .catch(err => res.send(err))
-    });
+    app.get('/closestStops', closestStops);
     app.get('/index.html', (req, res) => {
         res.sendFile('index.html', options)
     });
     app.get('/script.js', (req, res) => {
         res.sendFile('script.js', options)
+    });
+    app.get('/style.css', (req, res) => {
+        res.sendFile('style.css', options)
     });
 
     app.listen(3000, () => console.log('BusBoard listening on port 3000!'))
